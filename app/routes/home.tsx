1289fa2aa68e3b1f089b05d0d52fcde04a1c9ab2
Gong Moonphruk <gong.moonphruk@gmail.com>
2026-04-15 15:08:38 +0200

    Enhance ResumeCard component with conditional rendering for company name and job title, and update image handling to use generated URL.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,8 @@
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePuterStore } from "~/lib/puter";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
@@ -15,12 +15,38 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { auth } = usePuterStore();
+  const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
 
   useEffect(() => {
     if (!auth.isAuthenticated) navigate("/auth?next=/");
   }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      const items = (await kv.list("*", true)) as KVItem[];
+
+      const parsedResumes = items
+        ?.map((item) => {
+          const json = item.value.startsWith("resume:")
+            ? item.value.slice(7)
+            : item.value;
+          try {
+            return JSON.parse(json) as Resume;
+          } catch {
+            return null;
+          }
+        })
+        .filter((item): item is Resume => item !== null)
+        .slice(0, 6);
+
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    };
+    loadResumes();
+  }, [kv]);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -28,10 +54,19 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
-          <h2>Review your submissions and check AI-powered feedback.</h2>
+
+          {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+          ) : (
+            <h2>Review your submissions and check AI-powered feedback.</h2>
+          )}
         </div>
 
-        {resumes.length > 0 && (
+        {loadingResumes && (
+          <img src="/images/resume-scan-2.gif" className="w-50" />
+        )}
+
+        {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
